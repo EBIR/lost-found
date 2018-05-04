df4c296e98099357f9320bf7daa430e37435157b
Eduardo González Olea <laloolea18@gmail.com>
2018-05-04 13:55:27 -0700

    Agregue estilo al index bonito

--- index.php
+++ index.php
@@ -11,36 +11,66 @@
 </head>
 <body>
 
-<div class="container">
-  <h1>Lost&Found Unison</h1>
+<div class="container-fluid bg-1 text-center" style=" max-height: 24vh; overflow:hidden;">
   
+     <div class="logo">
+      <img src="img/logo.jpg"  style= "height: 24vh;" alt="logo" class = "logoLost">
+     </div>
+
+    <div class="form-group name">
+      <label for="usr">Name:</label>
+      <input type="text" class="form-control" id="usr">
+    </div>
+    <div class="form-group password">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd">
+    </div> 
+
+
+
+    <div class="btn-group grupo">
+      <a href="home.php"><button type="button" class="btn btn-success login" id="log">Iniciar Sesión</button>.</a>
+      <a href="userform.php"><button type="button" class="btn btn-warning login">Registrarse</button>.</a>
+
+    </div>
+
+ 
 </div>
 
-<div class="form-group name">
-  <label for="usr">Name:</label>
-  <input type="text" class="form-control" id="usr">
-</div>
-<div class="form-group password">
-  <label for="pwd">Password:</label>
-  <input type="password" class="form-control" id="pwd">
-</div> 
-
-
-<div class="btn-group grupo">
-  <a href="home.php"><button type="button" class="btn btn-success login" id="log">Iniciar Sesión</button>.</a>
-  <a href="userform.php"><button type="button" class="btn btn-warning login">Registrarse</button>.</a>
-
-</div>
 
 <div>
   
-  <img id= "logo" src="unison.png" alt= "Unison">
+  <img id= "logo" src="unison.png" alt= "Unison" style=" height: 24vh;object-fit: contain;">
 </div>
- 
+<!-- Second Container -->
+<div class="container-fluid bg-2 text-center" style=" width:100%; height:60vh;">
+  <h3 class="margin">Quienes Somos?</h3>
+  <p>Somos un grupo de estudiantes de la Universidad de Sonora, que nos preocupamos por la integridad de nuestra institución. </p>
+  <!--<img src="img/uni.jpg" class="img-responsive img-circle margin fotito" style="display:inline" alt="Unison" width="500" height="350">-->
+  <div class="col-sm-4"> 
+      
+      <img src="img/uni.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+  </div>
+   <div class="col-sm-4"> 
+      <img src="img/lostandfound.png" class="img-responsive margin" style="width:115% height:100%" alt="Image">
+      
+  </div>
+  <div class="col-sm-4"> 
+      <img src="img/amigos.png" class="img-responsive margin" style="width:100%" alt="Image">
+      
+  </div>
+</div>
 
 
+<!-- Footer -->
+<footer class="container-fluid bg-4 text-center" style="height:16vh; ">
+  <p>Universidad de Sonora - EBIR</p> 
+  
+</footer>
+
+<!--
 <a href="colaboradores.php" class="btn btn-info colaboradores" role="button">Colaboradores </a>
-
+-->
 
 </body>
 </html>
