1678199103358f0b6accc224370b937bc5844dbd
Brayan Durazo <brayan9612@gmail.com>
2018-05-24 10:46:24 -0700

    Modificacion formato publication

--- publication.php
+++ publication.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Publicación</title>
-</head>
-<body>
-	<?php 
-	session_start();
+<?php
+		session_start();
 	if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == FALSE) {
 	  header('Location: index.php');
 	}
@@ -17,6 +11,40 @@
 	  header('Location: index.php');
 	}
 	$_SESSION['LAST_ACTIVITY'] = $time;
+
+	?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Publicación</title>
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	  <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+	
+	<header class="container-fluid bg-1 text-center" style="max-height: 18vh; overflow:hidden;">
+     <div class="logo">
+      <img src="img/logo.jpg"  style= "height: 18vh;" alt="logo" class = "logoLost">
+     </div>
+<!--
+     <div class="btn-group grupo">
+      <a href="index.php"><button type="button" class="btn btn-danger salir" >Salir</button></a>
+    </div>-->
+	</header>
+
+	<div class="col-sm-2 sidenav" style="background-color: #8c8c8c; width:auto;">
+      <a href="home.php"><button type="button" class="btn btn-success">Regresar</button></a>
+      
+      <a href="logout.php"><button type="button" class="btn btn-danger" style="width:auto; position:absolute;">Salir</button></a>
+     
+	</div>
+
+	<div class="container-fluid bg-2 text-center" style=" width:70%; height:100vh;">
+	<?php 
+	
 
 	include 'utility.php';
 
@@ -78,6 +106,17 @@
 		header('Location: home.php');
 	}
 	?>
+</div> 
+
+      
+     
+</div>
+
+	<footer class="container-fluid bg-4 text-center" style="height:20vh; position:bottom; ">
+ 	 <p style="font-size: 25px;">Universidad de Sonora</p> 
+   	<img src="img/ebir.jpg" class="img-responsive margin" style=" position:relative;   width:8%; left:46%;" alt="ebir">
+	</footer>
+
 
 </body>
 </html>
